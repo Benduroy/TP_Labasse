de5d8a76b75f2b64b34509f50ddc64e333b13caa
Alexis ROCHE <info.aroche@gmail.com>
2013-03-10 22:25:12 +0100

    Commit du projet

---
+++ ConsoleApplication1/ClassLibrary1/FootLibrary.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootLibrary
+{
+    public class Match
+    {
+        private Club ClubAway;
+        private Club ClubHome;
+        private int GoalsAway;
+        private int GoalsHome;
+        private Boolean forfeitAway;
+        private Boolean forfeitHome;
+
+        public Match(Club _Away, Club _Home, Boolean _Awayforfeit, Boolean _Homeforfeit)
+        {
+            ClubAway = _Away;
+            ClubHome = _Home;
+            forfeitAway = _Awayforfeit;
+            forfeitHome = _Homeforfeit;
+        }
+
+        public Match(Club _Away,Club _Home,int _AwayGoals,int _HomeGoals)
+        {
+            ClubAway = _Away;
+            ClubHome = _Home;
+            GoalsAway = _AwayGoals;
+            GoalsHome = _HomeGoals;
+        }
+
+        public Club Away
+        {
+            get
+            {
+                return ClubAway;
+            }
+        }
+
+        public int AwayGoals
+        {
+            get
+            {
+                return GoalsAway;
+            }
+        }
+
+        public Club Home
+        {
+            get
+            {
+                return ClubHome;
+            }
+        }
+
+        public int HomeGoals
+        {
+            get
+            {
+                return GoalsHome;
+            }
+        }
+
+        public bool IsAwayForfeit
+        {
+            get
+            {
+                return forfeitAway;
+            }
+        }
+
+        public bool IsDraw
+        {
+            get
+            {
+                if (HomeGoals == AwayGoals)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool IsHomeForfeit
+        {
+            get
+            {
+                return forfeitHome;
+            }
+        }
+    }
+
+    public class Club
+    {
+        private String _name;
+
+        public Club(String name)
+        {
+            this._name = name;
+        }
+
+        public override String ToString()
+        {
+            return _name;
+        }
+    }
+
+    public abstract class PointSystem
+    {
+        private bool IsHome;
+        private ITotal Points;
+
+        public ITotal InitialPoints
+        {
+            get;
+        }
+
+        public ITotal GetPointsFromMatch(Match m, bool isHome)
+        {
+                return Points;
+        }
+
+        public interface ITotal : IComparable
+        {
+            public void Increment(ITotal with)
+            {
+                
+            }
+
+            public override String ToString()
+            {
+                return " ";
+            }
+        }
+    }
+
+    public class Ranking
+    {
+        private class RankingEntry : IComparable
+        {
+            private Club club;
+            private PointSystem.ITotal points;
+
+            public RankingEntry(Club club, PointSystem.ITotal points)
+            {
+                this.club = club;
+                this.points = points;
+            }
+            public PointSystem.ITotal Points
+            {
+                get { return this.points; }
+            }
+            public Club Club
+            {
+                get { return this.club; }
+            }
+
+            #region IComparable Membres
+
+            public int CompareTo(object obj)
+            {
+                return -this.points.CompareTo(((RankingEntry)obj).Points);
+            }
+
+            #endregion
+        }
+        private PointSystem system;
+        private RankingEntry[] entries;
+
+        public Ranking(PointSystem system, Club[] clubs)
+        {
+            this.system = system;
+            this.entries = new RankingEntry[clubs.Length];
+            for (int i = 0; i < clubs.Length; i++)
+                this.entries[i] = new RankingEntry(clubs[i], system.InitialPoints);
+        }
+        private RankingEntry EntryFromClub(Club c)
+        {
+            foreach (RankingEntry entry in entries)
+                if (entry.Club == c)
+                    return entry;
+            return null;
+        }
+        public void Register(Match m)
+        {
+            EntryFromClub(m.Home).Points.Increment(system.GetPointsFromMatch(m, true));
+            EntryFromClub(m.Away).Points.Increment(system.GetPointsFromMatch(m, false));
+            Array.Sort(entries);
+        }
+        public Club GetClub(int index)
+        {
+            return entries[index].Club;
+        }
+        public PointSystem.ITotal GetPoints(int index)
+        {
+            return entries[index].Points;
+        }
+        public PointSystem.ITotal GetPoints(Club club)
+        {
+            return EntryFromClub(club).Points;
+        }
+    }
+
+    public sealed class FrenchLeague1PointSystem
+    {
+        private static readonly Lazy<FrenchLeague1PointSystem> TheInstance = new Lazy<FrenchLeague1PointSystem>(() => new FrenchLeague1PointSystem());
+
+        private FrenchLeague1PointSystem()
+        {
+
+        }
+
+
+        public class PointTotal
+        {
+            private int goalaverage;
+            private int points;
+
+            public int CompareTo(object obj)
+            {
+                return points - ((PointTotal)obj).points;
+            }
+
+            public void Increment(PointSystem.ITotal with)
+            {
+                points = ((PointTotal)with).points;
+            }
+
+            public PointTotal()
+            {
+                points = 0;
+            }
+            public PointTotal(Match m, bool home)
+            {
+                points += m.HomeGoals - m.AwayGoals;
+            }
+            public string ToString()
+            {
+                return "";
+            }
+        }
+
+        public override PointTotal GetPointsFromMatch(Match m, bool isHome)
+        {
+            return new PointTotal(m, isHome);
+        }
+
+        public override PointTotal InitialPoints
+        {
+            get { 
+                return new PointTotal();
+                }
+        }
+    }
+
+   
+}
---
+++ ConsoleApplication1/TestUnitaireFoot/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FootLibrary;
+
+namespace TestUnitaireFoot
+{
+    [TestClass]
+    public class TestMatch
+    {
+        private Club ClubExterieur;
+        private Club ClubHome;
+
+        [TestInitialize]
+        public void Init()
+        {
+            ClubExterieur = new Club("EquipeExterieur");
+            ClubHome = new Club("EquipeInterieur");
+        }
+
+        [TestMethod]
+        public void TestAway()
+        {
+            Match UnMatch = new Match(ClubExterieur, ClubHome, false,true);
+            Assert.AreEqual(UnMatch.Away, ClubExterieur);
+        }
+
+        [TestMethod]
+        public void TestHome()
+        {
+            Match UnMatch = new Match(ClubExterieur, ClubHome, false,true);
+            Assert.AreEqual(UnMatch.Home, ClubHome);
+        }
+        [TestMethod]
+        public void AwayGoals()
+        {
+            Match UnMatch = new Match(ClubExterieur, ClubHome, 1, 0);
+            Assert.AreEqual(UnMatch.AwayGoals, 1);
+        }
+
+        [TestMethod]
+        public void HomeGoals()
+        {
+            Match UnMatch = new Match(ClubExterieur, ClubHome, 0, 1);
+            Assert.AreEqual(UnMatch.HomeGoals, 1);
+        }
+
+        [TestMethod]
+        public void IsAwayForfeit()
+        {
+            Match UnMatch = new Match(ClubExterieur, ClubHome, true, false);
+            Assert.AreEqual(UnMatch.IsAwayForfeit, true);
+        }
+
+        [TestMethod]
+        public void IsDraw()
+        {
+            Match UnMatch = new Match(ClubExterieur, ClubHome, 1, 1);
+            Assert.AreEqual(UnMatch.IsDraw, true);
+        }
+        [TestMethod]
+        public void IsHomeForfeit()
+        {
+            Match UnMatch = new Match(ClubExterieur, ClubHome, false, true);
+            Assert.AreEqual(UnMatch.IsHomeForfeit, true);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            Club UnClub = new Club("UnSuperClubDeLaMortQuiTueDuTrololo");
+            Assert.AreEqual(UnClub.ToString(), "UnSuperClubDeLaMortQuiTueDuTrololo");
+        }
+
+    }
+
+
+}
